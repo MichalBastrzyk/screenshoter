b09001a72c19659dc519a64e9b13440d0c4748ac
Michał Bastrzyk <michal.bastrzyk.pl@gmail.com>
2025-04-07 12:18:35 +0200

    Add installation script for screenshoter and update build command in package.json

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -r ./dist/screenshoter $HOME/.local/bin/screenshoter
+
+echo "Screenshoter installed successfully!"
+echo "You can now use the screenshoter command to take screenshots."
